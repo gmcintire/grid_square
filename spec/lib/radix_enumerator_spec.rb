671ddaa5e9124423f325da1fbf1c278d96bc703f
Graham McIntire <gmcintire@gmail.com>
2012-11-17 08:33:54 -0600

    Pass all the tests!

--- spec/lib/radix_enumerator_spec.rb
+++ spec/lib/radix_enumerator_spec.rb
@@ -1,32 +1,30 @@
 require 'spec_helper'
 require_relative '../../lib/radix_enumerator'
 
-module GridSquare
-  describe RadixEnumerator do
-    Given(:radix) { RadixEnumerator.new }
+describe GridSquare::RadixEnumerator do
+  Given(:radix) { GridSquare::RadixEnumerator.new }
 
-    Then {
-      radix.next[0].should == ?A.ord
-      radix.next[0].should == "0".ord
-      radix.next[0].should == ?A.ord
-      radix.next[0].should == "0".ord
-      radix.next[0].should == ?A.ord
-      radix.next[0].should == "0".ord
-      radix.next[0].should == ?A.ord
-      radix.next[0].should == "0".ord
-      radix.next[0].should == ?A.ord
-    }
+  Then {
+    radix.next[0].should == ?A.ord
+    radix.next[0].should == "0".ord
+    radix.next[0].should == ?A.ord
+    radix.next[0].should == "0".ord
+    radix.next[0].should == ?A.ord
+    radix.next[0].should == "0".ord
+    radix.next[0].should == ?A.ord
+    radix.next[0].should == "0".ord
+    radix.next[0].should == ?A.ord
+  }
 
-    Then {
-      radix.next[1].should == 18
-      radix.next[1].should == 10
-      radix.next[1].should == 24
-      radix.next[1].should == 10
-      radix.next[1].should == 24
-      radix.next[1].should == 10
-      radix.next[1].should == 24
-      radix.next[1].should == 10
-      radix.next[1].should == 24
-    }
-  end
+  Then {
+    radix.next[1].should == 18
+    radix.next[1].should == 10
+    radix.next[1].should == 24
+    radix.next[1].should == 10
+    radix.next[1].should == 24
+    radix.next[1].should == 10
+    radix.next[1].should == 24
+    radix.next[1].should == 10
+    radix.next[1].should == 24
+  }
 end
